b8f9c5800bb526234e96c3b6dd83e66286a31138
Erkan Colak <erkan@Kronos.intern>
2017-01-04 05:44:02 +0100

    adapted simple build script

--- src/make.sh
+++ src/make.sh
@@ -1,6 +1,7 @@
 
-rm ../release/knx2j.json
-rm ../release/knx2j.jar
+rm -f ../release/knx2j.json
+rm -f ../release/knx2j.jar
 
+javac knx2j.java
 jar cfm ../release/knx2j.jar manifest.mf knx2j.class G3m1n1S3rv3r/*.class com/eclipsesource/json/*.class
 cp knx2j.json ../release/knx2j.json
